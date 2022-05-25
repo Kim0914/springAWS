25f592964b3d94915d3f3feaaa1d60ca604b7f69
kim0914 <kosa0914@naver.com>
2022-05-25 22:28:27 +0900

    Feat: Add post create and read API
    글 작성 후 출력 로직 추가
    데이터베이스에 있는 모든 글 가져오는 쿼리 추가

---
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -0,0 +1,26 @@
+package org.example.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springboot.service.PostsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+}
